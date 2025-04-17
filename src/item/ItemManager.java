8d15f0c77141cd6b4dfba0b842837d4fad200f0c
Ankanpaa <4nkanpaa@gmail.com>
2025-04-17 16:44:46 +0300

    Items work in progress..

---
+++ src/item/ItemManager.java
@@ -0,0 +1,43 @@
+package item;
+
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import main.GamePanel;
+import main.UtilityTool;
+import tile.Tile;
+import main.GamePanel;
+
+public class ItemManager {
+
+    GamePanel gp;
+
+    public Item[] item;
+
+    public ItemManager(GamePanel gp) {
+
+        this.gp = gp;
+
+        item = new Item[10];
+
+        getItemImage();
+    }
+    public void getItemImage() {
+
+}
+public void setup(int itemID, String imageName, boolean collision) {
+        UtilityTool uTool = new UtilityTool();
+
+        try {
+
+            item[itemID] = new Item();
+            item[itemID].image = ImageIO.read(getClass().getResourceAsStream("/res/items/" + imageName + ".png"));
+            item[itemID].image = uTool.scaleImage(item[itemID].image, gp.tileSize, gp.tileSize);
+            System.out.println("Loaded image: " + imageName);
+
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
