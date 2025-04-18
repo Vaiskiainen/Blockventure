381676ee9def39e9386e978c1a7c7399e5a8bc5d
Ankanpaa <151853601+Ankanpaa@users.noreply.github.com>
2025-04-18 12:31:32 +0300

    Add files via upload

---
+++ src/object/OBJ_Boots.java
@@ -0,0 +1,30 @@
+package object;
+
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import main.GamePanel;
+
+public class OBJ_Boots extends SuperObject {
+
+    GamePanel gp;
+
+    public OBJ_Boots(GamePanel gp) {
+
+        this.gp = gp;
+        
+        name = "Boots of Swiftness";
+        try {
+
+            image = ImageIO.read(getClass().getResourceAsStream("/res/objects/boots_of_swiftness.png"));
+            uTool.scaleImage(image, gp.tileSize, gp.tileSize);
+            System.out.println("Loaded image: Boots of Swiftness");
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+            
+    }
+
+}
